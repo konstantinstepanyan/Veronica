7b7d8cfc5a54f3f46beb3538ac969cf77a09bc07
Konstantin <konstantinstepanyan99@gmail.com>
2022-07-20 00:59:25 +0300

    1

--- dev/assets/js/secondScripts.js
+++ dev/assets/js/secondScripts.js
@@ -2,18 +2,14 @@ import { a, Slider } from './vars.js';
 import { Popup } from './libs/popup.js';
 import { appendToHead } from './libs/appendToHead.js';
 
-
 //в JS можно использовать импорты, чтобы переменные, объекты и классы хранить в одном файле,
 //а использовать их - в другом
-
 
 //такая запись работает если запускать через OpenServer, если залить на хостинг 
 //ИЛИ если запустить через браузер с отключённым CORS.
 //Включённый CORS блокирует импорты в локальных файлах и разрешает импорты только через http или другие схемы.
 //Отключать CORS не безопасно
 //в файл, в который идёт импорт другого файла, добавить type='module'
-
-
 document.addEventListener('DOMContentLoaded', () => {
 
 
@@ -39,5 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
         //autocloseTime: 2000, //ms
         closeOnMiss: true,
         closeOnKeys: '27, 67, 88' //esc 27, c - 67, x - 88
+    });
+
+    $('.slider').slick({
+        dots: true, arrows: true
     });
 });
